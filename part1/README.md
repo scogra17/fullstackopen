2057f06f79cbc120ca1cdd368e989bb9b328fff6
Scott Graham <scttgrhm7+public@gmail.com>
2022-02-08 10:40:49 -0500

    Complete exercise 1.3

--- part1/README.md
+++ part1/README.md
@@ -11,6 +11,18 @@ Notes for part 1 of the course
 ## Passing data to components
 * Data is passed as a prop object
 
+## Immutability
+* Favor methods that do **not** modify objects in place. The functional paradigm encouraged by React has the characteristic of using immutable data structures (e.g. use Array.prototype.concat rather than Array.prototype.push)
+
+## JS Syntax reminders
+### Destructuring assignment
+```javascript
+const t = [1, 2, 3, 4, 5]
+const [first, second, ...rest] = t
+console.log(first, second)  // 1, 2 is printed
+console.log(rest)          // [3, 4, 5] is printed
+```
+
 ## Misc
 * A component should return either a single root node or an array. The root node, however, can be an empty tag ("fragment") to prevent too many divs from being in the markup
 * A component name must be capitalized
--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -15,26 +15,26 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts.part1}/>
-      <Part part={props.parts.part2}/>
-      <Part part={props.parts.part3}/>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.parts.part1.exercises + props.parts.part2.exercises + props.parts.part3.exercises}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = {
-    part1: {name: 'Fundamentals of React', exercises: 10},
-    part2: {name: 'Using props to pass data', exercises: 7},
-    part3: {name: 'State of a component', exercises: 14},
-  }
+  const parts = [
+    {name: 'Fundamentals of React', exercises: 10},
+    {name: 'Using props to pass data', exercises: 7},
+    {name: 'State of a component', exercises: 14},
+  ]
 
   return (
     <div>
