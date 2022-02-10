3b816f99aef3a0bcadcfc8bbbdc4ec4ff255e9d8
Scott Graham <scttgrhm7+public@gmail.com>
2022-02-10 13:56:12 -0500

    Complete exercise 2.4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,11 +1,10 @@
 const Header = ({ course }) => {
   return (
-    <h1>{course.name}</h1>
+    <h2>{course.name}</h2>
   )
 }
 
 const Total = ({ course }) => {
-  console.log(course)
   const sum = course.parts.reduce((acc, val) => {
       return acc + val.exercises
     }, 0)
@@ -69,8 +68,56 @@ const App = () => {
       },
     ]
   }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  console.log(courses)
   return (
-    <Course course={course} />
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => <Course course={course}/>)}
+    </>
   )
 }
 
