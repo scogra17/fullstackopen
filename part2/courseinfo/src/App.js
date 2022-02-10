93ab622828a8df72df81d09c6757d2d04b4e8794
Scott Graham <scttgrhm7+public@gmail.com>
2022-02-10 13:35:31 -0500

    Complete exercise 2.2

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -5,9 +5,14 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  console.log(course)
+  const sum = course.parts.reduce((acc, val) => {
+      return acc + val.exercises
+    }, 0)
   return(
-    <p>Number of exercises {sum}</p>
+    <p>
+      <b>total of {sum} exercises</b>
+    </p>
   )
 }
 
@@ -32,6 +37,7 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course}/>
     </div>
   )
 }
@@ -55,6 +61,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       },
     ]
   }
