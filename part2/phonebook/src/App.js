67b77c48e27731ad2ec36aa9299e36f9781ed79c
Scott Graham <scttgrhm7+public@gmail.com>
2022-02-11 16:10:31 -0500

    Complete exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -12,9 +12,13 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = {name: newName}
-    setPersons(persons.concat(newPerson))
-    setNewName('')
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to your phonebook`)
+    } else {
+      const newPerson = {name: newName}
+      setPersons(persons.concat(newPerson))
+      setNewName('')
+    }
   }
 
   return (
