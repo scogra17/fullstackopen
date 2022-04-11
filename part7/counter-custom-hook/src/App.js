ced0455443a0eb9b82e3ef3341409f1d058840ff
Scott Graham <scttgrhm7+public@gmail.com>
2022-04-11 13:44:15 -0700

    Separate counter into own module

--- part7/counter-custom-hook/src/App.js
+++ part7/counter-custom-hook/src/App.js
@@ -1,27 +1,4 @@
-import { useState } from 'react'
-
-const useCounter = () => {
-  const [value, setValue] = useState(0)
-
-  const increase = () => {
-    setValue(value + 1)
-  }
-
-  const decrease = () => {
-    setValue(value - 1)
-  }
-
-  const zero = () => {
-    setValue(0)
-  }
-
-  return {
-    value,
-    increase,
-    decrease,
-    zero
-  }
-}
+import useCounter from './hooks/counter'
 
 const App = (props) => {
   const { value, increase, decrease, zero } = useCounter()
